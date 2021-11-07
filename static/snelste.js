201ac44f21d45cfc0a31fe956dea894b5e7a3790
Jan-Willem Wilmsen <janwillemwilmsen@gmail.com>
2021-11-07 16:42:00 +0100

    script-new

--- static/snelste.js
+++ static/snelste.js
@@ -1,15 +1,26 @@
-var makeIframe = document.createElement("iframe");
-makeIframe.setAttribute("src", "http://aol.com");
-makeIframe.setAttribute("scrolling", "no");
-makeIframe.style.border = "none";
-makeIframe.style.left =  "-453px";
-makeIframe.style.top = "-70px";
-makeIframe.style.position = "absolute";
-makeIframe.style.width = "1440px";
-makeIframe.style.height = "775px";
+// var makeIframe = document.createElement("iframe");
+// makeIframe.setAttribute("src", "http://aol.com");
+// makeIframe.setAttribute("scrolling", "no");
+// makeIframe.style.border = "none";
+// makeIframe.style.left =  "-453px";
+// makeIframe.style.top = "-70px";
+// makeIframe.style.position = "absolute";
+// makeIframe.style.width = "1440px";
+// makeIframe.style.height = "775px";
 
-var makediv = document.createElement("div");
-makediv.style.height = "43px";
-makediv.style.width = "564px";
-makediv.style.position = "relative";
-makediv.style.overflow = "hidden";
+// var makediv = document.createElement("div");
+// makediv.style.height = "43px";
+// makediv.style.width = "564px";
+// makediv.style.position = "relative";
+// makediv.style.overflow = "hidden";
+
+let li = document.createElement('li');
+li.textContent = 'Products';
+menu.appendChild(li);
+
+li = document.createElement('li');
+li.textContent = 'About Us';
+
+// select the ul menu element
+const menu = document.querySelector('#menu');
+menu.appendChild(li);
