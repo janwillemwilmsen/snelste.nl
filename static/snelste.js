473e9c2eaeac5f0980d128b92903eccf243cc7b7
Jan-Willem Wilmsen <janwillemwilmsen@gmail.com>
2021-11-07 19:38:25 +0100

    third script

--- static/snelste.js
+++ static/snelste.js
@@ -1,26 +1,17 @@
-// var makeIframe = document.createElement("iframe");
-// makeIframe.setAttribute("src", "http://aol.com");
-// makeIframe.setAttribute("scrolling", "no");
-// makeIframe.style.border = "none";
-// makeIframe.style.left =  "-453px";
-// makeIframe.style.top = "-70px";
-// makeIframe.style.position = "absolute";
-// makeIframe.style.width = "1440px";
-// makeIframe.style.height = "775px";
-
-// var makediv = document.createElement("div");
-// makediv.style.height = "43px";
-// makediv.style.width = "564px";
-// makediv.style.position = "relative";
-// makediv.style.overflow = "hidden";
-
-// let li = document.createElement('li');
-// li.textContent = 'Products';
-// menu.appendChild(li);
-
-// li = document.createElement('li');
-// li.textContent = 'About Us';
-
-// // select the ul menu element
-// const menu = document.querySelector('#menu');
-// menu.appendChild(li);
+<script>
+(function() {
+  // Create a new H3 element
+  var h3 = document.createElement('h3');
+  
+  // Add the text content
+  h3.innerText = "It's really cool - I beloof!";
+  
+  // Get the reference to the current heading
+  var title = document.querySelector('p.speedlify-subhed');
+  
+  // Inject the new element right after the H1
+  if (title) {
+    title.parentElement.insertBefore(h3, title.nextSibling);
+  }
+})();
+</script>
