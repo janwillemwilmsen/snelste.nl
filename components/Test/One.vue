72a8b2ae27dbbdcad6d59b166da4f21ee1f71bb5
Jan-Willem Wilmsen <janwillemwilmsen@gmail.com>
2021-12-31 18:08:27 +0100

    testpageCreated

--- components/Test/One.vue
+++ components/Test/One.vue
@@ -4,7 +4,7 @@
 
 	<div class="flex flex-col items-stretch h-full max-w-xl mx-auto">
 			<h2 class="text-lg nieuwstitel ">{{ title }}</h2>
-     <p class="h-full nieuwssubtitel"> {{ subtitle }} {{ url }} {{ url }} <!--- {{ url() }}  method ---></p>
+     <p class="h-full nieuwssubtitel"> {{ subtitle }}  <!---{{ this.url }} {{ url }} {{ url() }}  method ---></p>
 
 <keep-alive>
 <!-- <a target="_blank" :href="publicPath + $route.query.url" class="block px-4 py-1 m-6 mx-auto text-base font-medium text-center text-white break-all transition-colors duration-500 transform bg-gray-400 border border-gray-600 rounded md:w-64 hover:bg-gray-500 md:mt-0"> &raquo; Test {{$route.query.url }}  </a> -->
@@ -12,7 +12,7 @@
 <!-- <a target="_blank" :href="publicPath + this.$route.params.url" class="block px-4 py-1 m-6 mx-auto text-base font-medium text-center text-white break-all transition-colors duration-500 transform bg-gray-400 border border-gray-600 rounded md:w-64 hover:bg-gray-500 md:mt-0"> &raquo; Test {{$route.query.url }}  </a> -->
 <!-- <a target="_blank" :href="publicPath + params.url" class="block px-4 py-1 m-6 mx-auto text-base font-medium text-center text-white break-all transition-colors duration-500 transform bg-gray-400 border border-gray-600 rounded md:w-64 hover:bg-gray-500 md:mt-0"> &raquo; Test {{$route.query.url }}  </a> -->
 <!-- <a target="_blank" :href="publicPath + this.url_data" class="block px-4 py-1 m-6 mx-auto text-base font-medium text-center text-white break-all transition-colors duration-500 transform bg-gray-400 border border-gray-600 rounded md:w-64 hover:bg-gray-500 md:mt-0"> &raquo; Test {{$route.query.url }}  </a> -->
-<a target="_blank" :href="publicPath + $route.query.url" class="block px-4 py-1 m-6 mx-auto text-base font-medium text-center text-white break-all transition-colors duration-500 transform bg-gray-400 border border-gray-600 rounded md:w-64 hover:bg-gray-500 md:mt-0"> &raquo; Test {{ $route.query.url }}  </a>
+<a target="_blank" :href="publicPath + this.$route.query.url" class="block px-4 py-1 m-6 mx-auto text-base font-medium text-center text-white break-all transition-colors duration-500 transform bg-gray-400 border border-gray-600 rounded md:w-64 hover:bg-gray-500 md:mt-0"> &raquo; Test {{ $route.query.url }}  </a>
 </keep-alive>
 
    <!-- <table class="newstable">
@@ -41,25 +41,37 @@
 
 
   export default {
- watchQuery: ['url'],
-
-
-
-    name: 'url',
+    watchQuery: ['url'],
+    // name: 'url',
     data(){
       return {
-        // url: this.$route.query.url,
+        url: this.$route.query.url,
         // url_data: this.$route.params.url,
-    //  url: $params,
+        //  url: $params,
         publicPath: "https://pagespeed.web.dev/report?url="
     }
       },
+
+
+ 
+
+      mounted() {
+    this.$nextTick(() => {
+      console.log(this.$route.query.url)
+      // undefined
+      console.log(this.$route.query.url)
+      console.log(this.$route)
+         
+      // {}
+    })
+  },
 
 // computed: {
 //       url() {
 //         return this.$route.query.url
 //       }
 //     },
+
 // watch: {
 //     url(newVal) {
 //         this.$router.push({ query: { ...this.$route.query, url: newVal } });
@@ -69,45 +81,49 @@
 //     }
 // },
 
-computed:{
-      url: {
-          get() {
-          return this.$route.query.url
-           },
-          set() {
-          return this.$route.query.url
-          } 
-         }
-    },
-
-  
+// computed:{
+//       url: {
+//           get() {
+//           return this.$route.query.url
+//            },
+//           set() {
+//           return this.$route.query.url
+//           } 
+//          }
+//     },
 
 // asyncData({ route }) {
 //    console.log(route.query.url);
 //    console.log('asyncdata')
 // },
 
-  //   mounted() {
-  //   this.init()
-  //   this.url=this.$route.query.url
-  // },
+//   mounted() {
+//   this.init()
+//   this.url=this.$route.query.url
+// },
+
 // watch: {
 //  '$route.path': {
 //    handler () {
 //      let url = this.$route.params.url
 //    },
-  // methods: {
-  //   init () {
-  //     console.log(this.$route);  //should return object
-  //     console.log(this.$route.params); //should return object 
-  //     console.log(this.$route.params.url); //should return id of URL param 
-  //     console.log(this.$route.query);
-  //     // console.log(url_data2)
-  //     // console.log(this.$route.query);
-  //   },
-  // },
 
-     props: {
+// methods: {
+//   init () {
+//     console.log(this.$route);  //should return object
+//     console.log(this.$route.params); //should return object 
+//     console.log(this.$route.params.url); //should return id of URL param 
+//     console.log(this.$route.query);
+//     // console.log(url_data2)
+//     // console.log(this.$route.query);
+//   },
+// },
+
+props: {
+    //    url: {
+    //   type: String,
+    //   required: false,
+    // },
     title: {
       type: String,
       required: false,
---
+++ pages/error.vue
@@ -0,0 +1,51 @@
+<template>
+  <div style="padding:50px;">
+         
+         
+    <form ref="form" class="self-center h-12 mx-auto">
+        <!-- <input name="url"  :value="$route.query.url"> -->
+        <input name="url" v-model="url" placeholder="Vul je domeinnaam.nl of domein.com in" class="w-56 p-2 text-gray-900 border border-gray-300 rounded-lg md:w-72 bg-gray-50 sm:text-sm focus:ring-blue-500 focus:border-blue-500">
+        <!-- <a href="#" v-on:click="submit">SUBMIT</a> -->
+    </form>
+
+
+    <a target="_blank" :href="publicPath + this.$route.query.url" style=" background-color:gray;padding:10px;"> 
+    &raquo; Test {{ $route.query.url }} 
+    </a>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+   data(){
+   return {
+    url: this.$route.query.url,
+    publicPath: "https://pagespeed.web.dev/report?url="
+   
+    }
+     },  
+ methods : {
+    submit : function(){
+      this.$refs.form.submit()
+    }
+  },
+  created() {
+    console.log(this.$route.query.url); 
+  },
+
+watch: {
+    url(newVal) {
+        this.$router.push({ query: { ...this.$route.query, url: newVal } });
+    },
+    '$route.query.url': function(val) {
+        this.url = val;
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
